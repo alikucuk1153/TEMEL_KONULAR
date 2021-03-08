b398b56ff47bbba8b056fc80e88e06f1aa88c180
alikucuk1153@gmail.com <alikucuk1153@gmail.com>
2021-03-08 12:11:07 +0300

    ..

---
+++ TEMEL_KONULAR/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace METOTLAR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Product product1 = new Product();    //  instanse - class ın örneği denir.
+            product1.Adi = "Elma";
+            product1.Fiyat = 15;
+            product1.Aciklama = "Amasya Elması";
+
+            Product product2 = new Product();
+            product2.Adi = "Karpuz";
+            product2.Fiyat = 80;
+            product2.Aciklama = "Adana Karpuzu";
+
+            Product[] Products = new Product[] { product1, product2 };
+
+            foreach (Product product in Products)    // type safe  tip güvenliği
+            {
+                Console.WriteLine(product.Adi);
+                Console.WriteLine(product.Fiyat);
+                Console.WriteLine(product.Aciklama);
+                Console.WriteLine("--------------");
+            }
+
+            Console.WriteLine("-----Metot-----");
+
+            // reuse ability  -  metotlar ve fonksiyonlar birçok sayfada tekrar kullanma imkanı sağlamaktadır.
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(product1);
+            sepetManager.Ekle(product2);
+
+
+
+        }
+    }
+}
---
+++ TEMEL_KONULAR/SepetManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace METOTLAR
+{
+    class SepetManager
+    {
+       // naming convention 
+       // syntax
+       // (***) olayı fonksiyona parametre atama işlemi
+        public void Ekle(Product product)   //  metot yazılımı. () işareti var ise bu bir metot demektir.
+        {
+
+            Console.WriteLine("Sepete eklendi : " + product.Adi);
+        
+        }
+
+    }
+}
